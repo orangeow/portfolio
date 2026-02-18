553bd67d5c549c360e6589a0c60d5ce0f5bea243
orangeow <143812812+orangeow@users.noreply.github.com>
2026-02-17 23:09:09 -0500

    Add files via upload

---
+++ small-projects/script.js
@@ -0,0 +1,36 @@
+// let projects = document.getElementsByClassName("small-project");
+// let mainImages = document.getElementsByClassName("main-image");
+// let popOut = document.getElementById("pop-out");
+// let shade = document.getElementById("shade");
+
+// console.log(mainImages);
+
+// // for(let i=0; i<mainImages.length; i++){
+//     let otherImages = projects[0].getElementsByClassName("more-images");
+
+//     mainImages[0].onclick = function(e){
+      
+//             popOut.style.display = "flex";
+//             popOut.appendChild(mainImages[0]);
+
+//             console.log(document.body);
+//             shade.style.display = "block";
+
+//             console.log("big");
+
+//             // mainImages[0].classList.add("pop-out-image");
+
+//             popOut.onclick = function(f){
+//                 popOut.style.display = "none";
+//                 shade.style.display = "none";
+//                 popOut.innerHTML = "";
+
+//                 // mainImages[0].classList.remove("pop-out-image");
+
+//                 console.log(mainImages[0]);
+//                 mainImages[0].style.width = "100%";
+//                 console.log(mainImages);
+//             }
+
+//     }
+// // }
